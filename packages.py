1b22b3027d6ee3c86f4e3cf41e31ec6058247faf
Brad Kittrell <bradkittrell@gmail.com>
2026-03-01 16:42:50 -0500

    Add initial Jupyter Notebook file

--- packages.py
+++ packages.py
@@ -3,6 +3,9 @@ import os
 import pandas as pd
 import pickle
 import time
+import json
 import glob
 import numpy as np
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import seaborn as sns
